ff0bf2c706e36c275fdb327f93c219c62bd9ce23
spark0119 <sean@groupraise.com>
2019-11-20 10:56:42 -0300

    added 2 spaces

--- app/routes.py
+++ app/routes.py
@@ -6,10 +6,12 @@ from app.models import User, Weekly_Reflection
 from datetime import datetime
 from werkzeug.urls import url_parse
 
+
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html', title='Home')
+
 
 @app.route('/login', methods=['GET','POST'])
 def login():
@@ -28,10 +30,12 @@ def login():
         return redirect(next_page)
     return render_template('login.html', title='Sign in', form=form)
 
+
 @app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
 
 @app.route('/register', methods=['GET','POST'])
 def register():
@@ -47,6 +51,7 @@ def register():
         return redirect(url_for('login'))
     return render_template('register.html', title='Register',form=form)
 
+
 @app.route('/user/<username>')
 @login_required
 def user(username):
@@ -57,11 +62,13 @@ def user(username):
     ]
     return render_template('user.html',user=user, posts=posts)
 
+
 @app.before_request
 def before_request():
     if current_user.is_authenticated:
         current_user.last_seen = datetime.utcnow()
         db.session.commit()
+
 
 @app.route('/edit_profile', methods=['GET','POST'])
 @login_required
@@ -78,6 +85,7 @@ def edit_profile():
         form.about_me.data = current_user.about_me
     return render_template('edit_profile.html', title='Edit Procfile',
                            form=form)
+
 
 @app.route('/weekly_reflection', methods=['GET','POST'])
 @login_required
